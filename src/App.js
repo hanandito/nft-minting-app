597348f4f18f458c5e080da3f31a92ca42e9c205
hanandito <hananditos@gmail.com>
2022-03-16 22:00:59 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -217,7 +217,6 @@ function App() {
               backgroundColor: "var(--accent)",
               padding: 24,
               borderRadius: 24,
-              border: "4px dashed var(--secondary)",
               boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
             }}
           >
