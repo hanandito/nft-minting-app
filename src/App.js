7985bdd5bc58d41f5ae211b45ed726034d01df0c
hanandito <hananditos@gmail.com>
2022-03-17 10:35:11 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -75,8 +75,6 @@ export const StyledLogo = styled.img`
 `;
 
 export const StyledImg = styled.img`
-  box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
-  background-color: var(--accent);
   width: 200px;
   @media (min-width: 900px) {
     width: 250px;
